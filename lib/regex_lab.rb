3c294692269767b3637d464d6eca2a75a7569555
Bryan McAfee <brymcafee@gmail.com>
2018-04-18 14:43:11 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,9 +17,12 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/\A[A-Z]/)
-
-end
+  if text.match(/\A[A-Z]/)
+    true
+  else
+    false
+  end
+  end
 
 def valid_phone_number?(phone)
 
