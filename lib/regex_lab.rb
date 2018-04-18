45463e82816de6e3cb35825e9aa3cbc400b6d884
Bryan McAfee <brymcafee@gmail.com>
2018-04-18 14:16:14 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,10 +2,7 @@ def starts_with_a_vowel?(word)
 if word.scan(/\A[aeiouAEIOU]/)
   true
 else
-  if word.scan(/\A[^aeiouAEIOU)
   false
-end
-end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
